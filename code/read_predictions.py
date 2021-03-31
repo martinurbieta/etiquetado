d3c5150280232f27c669f5acb750b7b351fad50d
cololaborde <64672548+cololaborde@users.noreply.github.com>
2021-03-30 21:03:32 -0300

    Update read_predictions.py

--- code/read_predictions.py
+++ code/read_predictions.py
@@ -6,4 +6,9 @@ lines = archivo.readlines()
 
 for line in lines:
     dictionary = ast.literal_eval(line)
+    #Element name 
+    print(dictionay["elem"])
+    #points
     print(dictionary["points"])
+    #specific point [x1, x2, y1, y2]
+    print(dictionary["points"]["x1"])
