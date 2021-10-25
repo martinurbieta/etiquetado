0b3cccd44f5e2c46b7d2a3d284074c7ed1096216
cololaborde <tomasslaborde@hotmail.com>
2021-10-25 16:24:50 -0300

    group png labels

--- images/setArchitectureToy/all/extract_png_label.py
+++ images/setArchitectureToy/all/extract_png_label.py
@@ -3,11 +3,13 @@ import os
 
 for filename in os.listdir(os.getcwd()):
     if os.path.isdir(filename):
-        current_name = filename+"/label.png"
-        new_name = filename+"/"+filename+".png"
-        print(current_name)
-        print(new_name)
-        subprocess.run(["mv", new_name, current_name])
+        if(filename != "png_labels"):
+            current_name = filename+"/label.png"
+            new_name = filename+"/"+filename+".png"
+            #print(current_name)
+            print(new_name)
+            #subprocess.run(["mv", current_name, new_name])
+            subprocess.run(["cp", new_name, "png_labels"])
 
-        #subprocess.run(["labelme_json_to_dataset", filename])
+            #subprocess.run(["labelme_json_to_dataset", filename])
         
