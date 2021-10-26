810f4c7d8ce887357ed4011b66bb34887b8fcabb
cololaborde <tomasslaborde@hotmail.com>
2021-10-26 16:34:24 -0300

    sort

--- code/labelme_json_to_dataset.py
+++ code/labelme_json_to_dataset.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 
+#requiere tener labelme instalado
 for filename in os.listdir(os.getcwd()):
     if filename.endswith(".json"):
         subprocess.run(["labelme_json_to_dataset", filename])
